6f7fc48e9f10ffe0f190bd685c402f2362fcfd24
Lee Sung Guk <jin3137@gmail.com>
2019-03-28 00:20:01 +0900

    "인텔리제이에서 수정 commit 테스트"

--- src/main/java/com/spring/demo/DemoApplication.java
+++ src/main/java/com/spring/demo/DemoApplication.java
@@ -1,12 +1,20 @@
 package com.spring.demo;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(DemoApplication.class, args);
     }
 
